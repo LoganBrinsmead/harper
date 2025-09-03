2ef83779f09fcd461cb180613b47f846180df0b9
Elijah Potter <me@elijahpotter.dev>
2025-09-03 12:25:26 -0600

    feat(ripper): reduce gens

--- ripper/queue.sh
+++ ripper/queue.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-cargo run --release -- -m 1000000 -g 200 -c 10 --problem-file rules/to_too.txt --clean-file clean.txt --max-mutations 60 --seed to -o to_too.sol 
-cargo run --release -- -m 1000000 -g 200 -c 10 --problem-file rules/missing_article.txt --clean-file clean.txt --max-mutations 60 -o missing_article.sol
-cargo run --release -- -m 1000000 -g 200 -c 10 --problem-file rules/indp.txt --clean-file clean.txt --max-mutations 60 -o best.txt
+cargo run --release -- -m 1000000 -g 75 -c 10 --problem-file rules/to_too.txt --clean-file clean.txt --max-mutations 60 --seed to -o to_too.sol 
+cargo run --release -- -m 1000000 -g 75 -c 10 --problem-file rules/missing_article.txt --clean-file clean.txt --max-mutations 60 -o missing_article.sol
+cargo run --release -- -m 1000000 -g 75 -c 10 --problem-file rules/indp.txt --clean-file clean.txt --max-mutations 60 -o best.txt
 
